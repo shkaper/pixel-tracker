9bc0bee91ff0e9b0ce7bdeb3e67dd6a2453929d0
Nicolai Zaitsev <nicolai.zaitsev@revjet.com>
2016-02-16 22:31:43 -0400

    Added ngclipboard dependencies

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('pixelTrackerApp', ['ui.router', 'ngResource'])
+angular.module('pixelTrackerApp', ['ui.router', 'ngResource', 'ngclipboard'])
     .run(function($rootScope) {
         $rootScope.pageTitle = 'Home';
     })
