a5585adc46f8583f5c497f330c799335d6eec1b8
Nicolai Zaitsev <nicolai.zaitsev@revjet.com>
2016-03-03 23:56:50 -0400

    Updated pixels pagination

--- app/scripts/controllers/PixelCtrl.js
+++ app/scripts/controllers/PixelCtrl.js
@@ -75,6 +75,26 @@ angular.module('pixelTrackerApp')
                 });
         };
 
+        $scope.displayPages = function(currentPage, totalPages) {
+            var arr = [1];
+            for (var i = currentPage - 1; i <= currentPage + 1; ++i) {
+                if (i > 1 && i < totalPages) {
+                    arr.push(i);
+                }
+            }
+            if (arr.indexOf(totalPages) === -1) {
+                arr = arr.concat(totalPages);
+            }
+            if ((arr[1] - 1) > arr[0]) {
+                arr.splice(1, 0, "blank");
+            }
+            if ((arr[arr.length - 1] - 1) > arr[arr.length - 2]) {
+                arr.splice(arr.length - 1, 0, "blank");
+            }
+            console.log(arr);
+            return arr;
+        };
+
         //init table
 
         $scope.getPixelsPage($scope.pixelsPage, $scope.pixelsPerPage);
