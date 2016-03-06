16b277ee852586000e011b52a4b1e91738910bc0
Nicolai Zaitsev <nicolai.zaitsev@revjet.com>
2016-03-05 20:31:37 -0400

    Updated server for heroku

--- server.js
+++ server.js
@@ -1,35 +1,25 @@
 // modules =================================================
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var mongoose       = require('mongoose');
-var morgan         = require('morgan');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
 
 // configuration ===========================================
 
-// config files
-var db = require('./config/db');
-
-// set our port
-var port = process.env.PORT || 3001;
-
 // log every request to the console
 app.use(morgan('dev'));
-
-// connect to our mongoDB database
-// (uncomment after you enter in your own credentials in config/db.js)
- mongoose.connect(db.url);
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json
 app.use(bodyParser.json());
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override'));
@@ -40,12 +30,41 @@ app.use(express.static(__dirname + '/dist'));
 // routes ==================================================
 require('./server/routes')(app); // configure our routes
 
-// start app ===============================================
-// startup our app at http://localhost:3001
-app.listen(port);
+// connect to db and start app ===============================================
 
-// shoutout to the user
-console.log('Magic happens on port ' + port);
+var dbURI = process.env.MONGOLAB_URI || 'mongodb://localhost/pixels_temp';
+
+mongoose.connect(dbURI);
+
+mongoose.connection.once('connected', function () {
+    console.log('Mongoose default connection open to ' + dbURI);
+
+    // Initialize the app
+    var server = app.listen(process.env.PORT || 3001, function () {
+        var port = server.address().port;
+        console.log("Magic happens on port " + port);
+    });
+});
+
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+    process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+mongoose.connection.on('reconnected', function () {
+    console.log('Mongoose default connection re-established to ' + dbURI);
+});
+
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
 
 // expose app
 exports = module.exports = app;
